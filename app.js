f34b64abaa899e870d52d04fbba6386b4dae8d4d
aimanfatima248 <aimanfatima65@outlook.com>
2024-02-22 14:13:01 +0500

    add JS assignment 16

---
+++ app.js
@@ -0,0 +1,176 @@
+// Chap1
+// QS1
+// alert("Welcome to  our Website!")
+
+// QS2
+// alert("Error!Please enter a valid password")
+
+// QS3
+// alert("Welcome to JS land... \n Happy Coding")
+
+// QS4
+// alert("Welcome to JS Land...")
+// alert("Happy Coding! \n Prevent this page from creating additional dialogues")
+
+// QS5
+// alert("Hello!I can run JS through my web browser's console")
+
+// QS6
+// alert( "Thankyou for visiting our page")
+ 
+// QS7
+// script tags add in html file
+
+// CHAP2
+
+// QS1
+// var username="AIMAN";
+// document.write(username)
+
+// QS2
+// var myName= "AIMAN FATIMA ";
+// console.log(myName)
+
+// QS3
+// a)
+// var message="hello, hope you are fine!";
+// console.log(message)
+
+// b)
+// var message="Hello World"
+// console.log(message)
+
+// c)
+// var message="hello, hope you are fine!";
+// alert(message)
+
+// QS4
+// var studentname="Jhone Doe"
+// alert(studentname)
+
+
+// var age="15 years old "
+// alert(age)
+
+// var course=" Certified Mobile App Development "
+// alert(course)
+
+// QS5
+// var food=" PIZZA\n PIZZ \n PIZ \n PI \n P"
+// alert(food)
+
+// QS6
+// var email="aimanfatima65@outlook.com";
+// alert(email)
+
+// QS7
+// var b=" A smarter way to learn Javascript"
+// alert( "I am trying to learn from the book"+ b);
+
+// QS8
+// document.write("Yah! I can write HTML content trough JavaScript")
+
+// QS9
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+// document.write("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// CHAP3
+
+// QS1
+// var age= "I am 15 years old";
+// alert(age)
+
+// QS2
+// var visit="You have visited this website 14 times";
+// alert(visit)
+
+// QS3
+// var birthyear="1996";
+// document.write("My birth year is",  birthyear , " <br/> data type of my declared variable is number " ) 
+
+
+// QS4
+// var visitorname="John Doe";
+// var producttitle="T-shirts";
+// var quantity=5;
+// document.write("<b>" + visitorname + "</b>" ,  "  ordered" + " " + "<b>" +quantity+ "</b>" + " " + "<b>" + producttitle +"</b>" , " on XYZ clothing store")
+ 
+// CHAP 1-3
+
+// Chap1
+
+// QS1.
+// 1)
+// alert("AIMAN")
+
+// 2)
+// alert("FATIMA")
+
+// 3
+// alert("aimanfatima65@outlook.com")
+
+// 4)
+// alert("0323341614778")
+
+// 5)
+// alert("jdsjldkl")
+
+// QS2
+// alert("you are learning javascript");
+
+// QS3
+// alert("plz like and subscribe my page")
+ 
+// CHAP 2
+// QS1
+
+// var firstName= "aimanFatima";
+// console.log(firstName)
+
+// QS2
+
+// var a=
+// a="aiman"
+
+// QS3
+// var teamname="KARACHI";
+// alert(teamname)
+
+// QS4
+// var bestman="MY FATHER";
+
+// chap 3
+// QS1
+// var caseqty= 5;
+// console.log(caseqty)
+
+// QS2
+// var caseqty= 144;
+// console.log(caseqty)
+
+// QS3
+// var num= 9;
+// console.log(num)
+
+// QS4
+// var a=5+3;
+
+// console.log(a)
+
+// QS5
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal)
+
+// QS6
+
+// var a=8;
+// console.log(a+2)
+
+
+
+
+
+
